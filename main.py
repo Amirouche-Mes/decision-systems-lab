b5ad945741d824028cda0c36d119da80c5ff14ff
Amirouche.Mesrour <amirouche.mesrour@isatis-canada.com>
2026-08-30 14:04:54 -0400

    first draft taking back the keyboard coding after a while

--- main.py
+++ main.py
@@ -1,3 +1,99 @@
-from src.decision_systems_lab.functions import generate_booking_df
+from src.decision_systems_lab.functions import generate_booking_df, data_cont_leakage_detection, compute_iv_auc, make_split
+import pandas as pd
 
+# generate data
 df_booking = generate_booking_df()
+
+# preprocessing step
+print("shape:", df_booking.shape)
+print("target rate:", df_booking.converted.mean().round(4))
+print("duplicates:", df_booking.duplicated().sum())
+print("date range:", df_booking.click_ts.min(), "->", df_booking.click_ts.max())
+print("null columns:", (df_booking.isna().mean()*100).round(2).sort_values(ascending=False).head())
+
+df_booking = df_booking.drop_duplicates().reset_index(drop=True)
+target_rate = df_booking.converted.mean().round(4)
+
+na_columns_serie = (df_booking.isna().mean()*100).round(2).sort_values(ascending=False)
+num_cols = set(df_booking.select_dtypes(include='number').columns)
+cat_cols = df_booking.select_dtypes(include=["object", "str", "string", "category"]).columns.to_list()
+
+int_col_data_leakage = data_cont_leakage_detection(num_cols, df_booking, df_booking.converted, target_rate, na_columns_serie)
+
+
+cat_col_leakage_df_result = pd.Series(cat_cols, index=cat_cols).apply(
+    lambda col: compute_iv_auc(df_booking, col, target_col="converted")
+)
+
+cat_col_leakage_df_result = cat_col_leakage_df_result.sort_values(by="AUC", ascending=False)
+
+cat_col_leakage_df_result.head()
+
+# feature enginering 
+
+PRIOR_W = 20.0
+df_booking = df_booking.sort_values("click_ts").reset_index(drop=True)
+prior = df_booking.converted.mean()
+
+# expending, features to add with more valuable information 
+# this section could be replaced by the category_encoder library, but it doesn't handle the leakage of the conv variable
+# possible variables to add that might help, 
+# dynamique frequency: time since last click, clicks last 24h, clicks on sessions, inactivity_time, price_ratio = price / avgprice_for_geo
+# price per night, frequency_encoding per partner, cyclic hours hour_sin = sin(2p*hour/24), hour_cos=...
+# # we can also add the embedding o
+g = df_booking.groupby("partner_id")["converted"]
+df_booking["partner_rate"] = (g.cumsum() - df_booking.converted + prior * PRIOR_W)/(g.cumcount() + PRIOR_W)
+
+import polars as pl 
+
+df_pl = pl.from_pandas(df_booking)
+
+df_pl = df_pl.with_columns(
+    # time since the last click of the partner_id 
+    time_sice_last_click_pd=(
+        pl.col("click_ts") - pl.col("click_ts").shift(1).over("partner_id", order_by="click_ts")
+    ).dt.total_seconds().fill_null(-1),
+    clicks_last_1h_pd = pl.col("click_ts").count().over("partner_id", order_by="click_ts").fill_null(-1),
+    time_since_last_click_pd=(
+        pl.col("click_ts") - pl.col("click_ts").shift(1).over("geo", order_by="click_ts")
+    ).dt.total_seconds().fill_null(-1),
+    clicks_last_1h_geo = pl.col("click_ts").count().over("geo", order_by="click_ts").fill_null(-1)
+)
+
+df_booking = df_pl.to_pandas()
+
+# final step cleaning -> 
+df_booking = df_booking.drop(columns=["minutes_on_ota_site", "booking_amount"])
+
+# let's make the split 
+train, valid, test = make_split(df_booking, "click_ts", .7, .85)
+
+# trainlightgbm 
+import lightgbm as lgb 
+
+FEATS = ['partner_id', 'device', 'geo', 'price', 'lead_time_days',
+         'nights', 'past_click_30d', 'converted', 'time_sice_last_click_pd',
+         'clicks_last_1h_pd', 'time_since_last_click_pd', 'clicks_last_1h_geo']
+CAT = ["partener_id", "device","geo"]
+
+def prep(d, cats=None):
+    d = d[FEATS].copy()
+    for c in CAT:
+        d[c] = d[c].astype("category") if cats is None else pd.Categorical(d[c], categories=cats[c])
+    return d 
+Xtr = prep(train); cats = {c: Xtr[c].cat.categories for c in CAT}
+Xva, Xte = prep(valid, cats), prep(test, cats)
+
+# let's make the first training
+model = lgb.LGBMClassifier(n_estimators=400, learning_rate=.05, num_leaves=31, 
+                           min_child_samples=50, random_state=0, verbose=-1,
+                           class_weight="balanced")
+
+# 
+from sklearn.linear_model import LogisticRegression
+
+model_lgr = LogisticRegression(penalty="l2", C="1.0", solver="lbfgs", 
+                               max_iter=1000, random_state=42,)
+
+
+
--- src/decision_systems_lab/functions.py
+++ src/decision_systems_lab/functions.py
@@ -18,7 +18,7 @@ def generate_booking_df():
     n = 50_000
     start = pd.Timestamp("2026-01-01")
     ts = start + pd.to_timedelta(rng.uniform(0, 180, n), unit="D")
-    partner_ids = [f"P{i:03}" for i in range(60)]
+    partner_ids = [f"P{i:03d}" for i in range(60)]
     partner_eff = dict(zip(partner_ids, np.random.default_rng(2).normal(0, 0.5, 60)))
     partners = rng.choice(partner_ids, n, p=np.random.default_rng(1).dirichlet(np.ones(60)*0.6))
     device = rng.choice(["mobile", "desktop", "tablet"], n, p=[.62, .31, .07])
@@ -37,7 +37,7 @@ def generate_booking_df():
     #         geo, more weight to canadian leads
     # saisonality, cyclic variation during the year
     logit = (-3.4 + 0.8*(device == "desktop") + 0.55*np.log1p(past_clicks)
-            - 0.8*np.abs(np.log(price)-0.5) - 0.0228*lead_time
+            - 0.8*np.abs(np.log(price)-5.0) - 0.022*lead_time
             + 0.25*(geo=="CA") + np.array([partner_eff[p] for p in partners])
             + 0.15*np.sin(ts.dayofyear/58.0))
     p = 1/(1+np.exp(-logit)); y = rng.binomial(1, p)
@@ -60,5 +60,218 @@ def generate_booking_df():
 #def evaluate(model, X, y) -> dict: ...   # AUC, PR-AUC, Brier
 #def run_experiment(params) -> dict: ...  # assemble et retourne une ligne de tableau
 # make other models and run the experiments. 
+
+
+# let's check the quality the univariate AUC in the data
+from sklearn.metrics import roc_auc_score
+
+def data_cont_leakage_detection(num_cols, df, target, target_rate, na_columns_serie): 
+    target = df.converted
+
+    bound_null = (target_rate) * .25
+
+    list_col_leakage_null = []
+    list_col_leakage_auc = []
+
+    for col in df.columns:
+        null_pct = na_columns_serie.get(col, 0.0)
+        print(f"{col}, {null_pct}")
+        if (1-target_rate) - bound_null <= null_pct/100.0 <= (1-target_rate) + bound_null:
+            list_col_leakage_null.append(col)
+        if col in num_cols:
+            if col == "converted":
+                continue
+            col_data = df[col].rank(na_option='bottom')
+            auc = roc_auc_score(target, col_data)
+            print(f"{col:>22s} univariate AUC = {auc:.3f}")
+            if 0 <= auc <= .1 or .9 <= auc <= 1:
+                print(
+                    f"{col} is suspected to be filled at convergence time, AUC :{auc}"
+                )
+                list_col_leakage_auc.append(col)
+    # leakage potentiel 
+    leakage_intersection = list(set(list_col_leakage_null) | set(list_col_leakage_auc))
+    return leakage_intersection
+
+# compute the information value to include all type of variables
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.model_selection import cross_val_predict
+
+def compute_iv_auc(df, cat_col, target_col):
+    print("Processing categorical column:", cat_col)
+    s_cat = df[cat_col].astype("category")
+    df_count = df.groupby(s_cat, dropna=False)[target_col].agg(["count", "sum"])
+    df_count["goods"] = df_count["sum"]
+    df_count["bads"] = df_count["count"] - df_count["sum"]
+    # get the propotion of goods and bads
+    prop_goods = df_count["goods"] / df_count["goods"].sum()
+    prop_bads = df_count["bads"] / df_count["bads"].sum()
+    prop_goods = np.where(prop_goods == 0, .00001, prop_goods)
+    prop_bads = np.where(prop_bads == 0, .0001, prop_bads)
+    woe = np.log(prop_goods / prop_bads)
+    iv = np.sum((prop_goods - prop_bads) * woe)
+    # Calculate the AUC 
+    model = HistGradientBoostingClassifier(
+        max_iter=50, categorical_features=[True], random_state=42
+    )
+    y_probs = cross_val_predict(
+        model, s_cat.to_frame(), df[target_col], cv=5, method="predict_proba"
+    )[:, 1]
+    auc = roc_auc_score(df[target_col], y_probs)
+    return pd.Series({"IV": iv, "AUC": auc})
+
+def make_split(df, split_col, low_pct, high_pct):
+    c1, c2 = df[split_col].quantile(low_pct), df[split_col].quantile(high_pct)
+    train = df[df[split_col] <= c1]
+    valid = df[(df[split_col] > c1) & (df[split_col] <= c2)]
+    test = df[df[split_col] > c2]
+    for name, d in [("train", train), ("valid", valid), ("test", test)]:
+        print(f"{name:5s} {len(d):6,d} | {d.click_ts.min().date()} -> {d.click_ts.max().date()} | rate {d.converted.mean():.4f}")
+    return train, valid, test
+
+# train function lgbm 
+import lightgbm as lgb
+from sklearn.linear_model import LogisticRegression
+from sklearn.preprocessing import OneHotEncoder, TargetEncoder, StandardScaler, OrdinalEncoder
+from sklearn.pipeline import Pipeline
+from sklearn.compose import ColumnTransformer
+
+
+
+def train_lgbm(Xtr, ytr, Xva, yva, num_cols, cat_cols, **params):
+    Xtr_filtered = Xtr[cat_cols + num_cols].copy()
+    Xva_filtered = Xva[cat_cols + num_cols].copy()
+
+    default_params = {
+        "n_estimators":400,
+        "learning_rate":0.05,
+        "verbose":-1,
+        "num_leaves":31,
+        "min_child_samples":50,
+        "random_state":0,
+    }
+
+    params_f = {**default_params, **params}
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "cat",
+                OrdinalEncoder(
+                    handle_unknown="use_encoded_value",
+                    unknown_value=-1,
+                ),
+                cat_cols
+            ),
+            ("num", "passthrough", num_cols),
+        ]
+    )
+    lgb_model = lgb.LGBMClassifier(**params_f)
+
+    full_pipeline_lgbm = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            ("classifier", lgb_model)
+        ]
+    )
+    full_pipeline_lgbm.fit(Xtr_filtered, ytr)
+    pa_val = full_pipeline_lgbm.predict_proba(Xva_filtered)[:,1]
+    return full_pipeline_lgbm, pa_val
+
+def train_lgr(Xtr, ytr, Xva, yva, num_cols, low_card_cols, high_card_cols, **params):
+
+    default_param = {
+        "C":1.0,
+        "max_iter":1000,
+        "random_state":0,
+        "solver":"lbfgs",
+    }
+    params_f = {**default_param, **params}
+
+    preprocessor = ColumnTransformer(
+        transformers=[
+            (
+                "num",
+                StandardScaler(),
+                num_cols,
+            ),
+            (
+                "low_card",
+                OneHotEncoder(handle_unknown="ignore"),
+                low_card_cols,
+            ),
+            (
+                "high_card",
+                TargetEncoder(smooth="auto", cv=5),
+                high_card_cols
+            )
+        ]
+    )
+    full_pipeline_lgr = Pipeline(
+        steps=[
+            ("preprocessor", preprocessor),
+            (
+                "classifier",
+                LogisticRegression(**params_f)
+            )
+        ]
+            
+    )
+    full_pipeline_lgr.fit(Xtr, ytr)
+    pa_val = full_pipeline_lgr.predict_proba(Xva)[:, 1]
+    return full_pipeline_lgr, pa_val
+
+from sklearn.metrics import roc_auc_score, brier_score_loss, log_loss
+
+def evaluate(model, X, y):
+
+    p_te = model.predict_proba(X)[:, 1]
+    real_rate = float(np.mean(y))
+    p_mean = float(np.mean(p_te))
+    brier = float(brier_score_loss(y, p_te))
+    auc = float(roc_auc_score(y, p_te))
+    loss = float(log_loss(y, p_te))
+
+    return {
+        "real_rate": round(real_rate, 4),
+        "p_mean": round(p_mean, 4),
+        "brier": round(brier, 4),
+        "log_loss": round(loss, 4),
+        "auc": round(auc, 4),
+    }
+
+
+def run_experiment(name, model_name, 
+                   Xtr, 
+                   ytr,
+                   Xva, 
+                   yva, 
+                   Xte,
+                   yte,
+                   num_cols, 
+                   cat_cols, 
+                   low_card_cols,
+                   high_card_cols,
+                   # changing params
+                   model_params=None):
+    model_params = model_params or {}
+    
+    if model_name == "lgb":
+        model, p_val = train_lgbm(Xtr, ytr, Xva, yva, num_cols, cat_cols, **model_params)
+    elif model_name == "lgr":
+        model, p_val = train_lgr(Xtr, ytr, Xva, yva, num_cols, low_card_cols, high_card_cols, **model_params)
+        
+    else:  
+         raise ValueError(
+                    f"Model {model_name} is not handled. Possible choices: 'lbg', 'lgr'"
+                )
+    metrics = evaluate(model, Xte, yte)
+
+    return {"experiment name": name, 
+            "model_name": model_name, 
+            "metrics": metrics,
+            **model_params}
+
+
 
 
